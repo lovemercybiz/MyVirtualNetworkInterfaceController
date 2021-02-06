c9ef6cae35d26aab5b76ba31bb874fd4de7e37c6
pc <desktop@x86.nt100>
2021-02-06 14:11:30 +0800

    m

--- translate_fstring.bat
+++ translate_fstring.bat
@@ -1,14 +1,1 @@
-@echo off
-setlocal enabledelayedexpansion
-for /f "delims=" %%i in ('dir "%~dp0*.py" /s /b /a:-d') do (
-    :choose_tmp_fil
-    set "tmp_fil=%%~dpni.!random!.py"
-    if exist "!tmp_fil!" (
-        goto choose_tmp_fil
-    )
-    future-fstrings-show "%%i">"!tmp_fil!"
-    set "bsn_fil=%%~nxi"
-    del /f /q "%%i"
-    ren "!tmp_fil!" "!bsn_fil!"
-    echo="%%i has been translated."
-)
+../MySocks5Server/translate_fstring.bat
